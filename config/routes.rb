c35011e702e7f671019b155b4a63e8c0e303639d
owenabbott <ocabbott@gmail.com>
2020-04-08 18:35:01 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :coupons
+
+  get '/coupons/index', to: 'coupons#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
